dd172d0ee63ab702bc262b8a75e52a12d24c5af8
Fernando Duarte Salhani <fduartesalhani@gmail.com>
2021-09-20 21:27:47 -0300

    alter ON CONFLICT clause for user inserts

--- sql_queries.py
+++ sql_queries.py
@@ -74,7 +74,7 @@ INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_i
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
   VALUES (%s, %s, %s, %s, %s)
-  ON CONFLICT (user_id) DO NOTHING;
+  ON CONFLICT (user_id) DO UPDATE SET level = excluded.level;
 """)
 
 song_table_insert = ("""
