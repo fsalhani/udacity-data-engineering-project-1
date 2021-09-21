e40652132579a47b75f02e7334843b38a2a6e1f8
Fernando Duarte Salhani <fduartesalhani@gmail.com>
2021-09-20 21:30:12 -0300

    Requested changes from code review (#1)
    * add not null constraints to songplays
    
    * add not null constraints to songs
    
    * alter ON CONFLICT clause for user inserts

--- sql_queries.py
+++ sql_queries.py
@@ -11,8 +11,8 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 songplay_table_create = ("""
 CREATE TABLE songplays (
   songplay_id    SERIAL PRIMARY KEY,
-  start_time     TIMESTAMP,
-  user_id        INT,
+  start_time     TIMESTAMP NOT NULL,
+  user_id        INT NOT NULL,
   level          VARCHAR,
   song_id        VARCHAR,
   artist_id      VARCHAR,
@@ -36,7 +36,7 @@ song_table_create = ("""
 CREATE TABLE songs (
   song_id        VARCHAR PRIMARY KEY,
   title          VARCHAR,
-  artist_id      VARCHAR,
+  artist_id      VARCHAR NOT NULL,
   year           INT,
   duration       FLOAT
 );
@@ -74,7 +74,7 @@ INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_i
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
   VALUES (%s, %s, %s, %s, %s)
-  ON CONFLICT (user_id) DO NOTHING;
+  ON CONFLICT (user_id) DO UPDATE SET level = excluded.level;
 """)
 
 song_table_insert = ("""
